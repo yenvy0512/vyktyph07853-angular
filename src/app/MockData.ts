71a92d550553422702f30481ed46d77e2b67ef3b
yenvy0512 <kieuyenvy0512@gmail.com>
2020-03-19 21:30:22 +0700

    1 file modified

--- src/app/MockData.ts
+++ src/app/MockData.ts
@@ -2,19 +2,50 @@
 import { Product } from './Product';
 export const data : Product[] = [
     {
-      id: 1,
-      name: "Item 1",
-      price: 24.99,
-      desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!',
-      status: false,
-      img: 'https://dummyimage.com/275x175/000/fff'
-    },
-    {
-      id: 2,
-      name: "Item 2",
-      price: 24.99,
-      desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!',
-      status: false,
-      img: 'https://dummyimage.com/275x175/000/fff'
-    }
+        id: 1,
+        name: "1",
+        price: 24,
+        desc:'Lorme',
+        status:false,
+        img:'https://techland.com.vn/wp-content/uploads/2019/09/apple-iphone-11-7.jpg'
+      },
+      {
+        id: 3,
+        name: "2",
+        price: 25,
+        desc:'Lorme',
+        status:true,
+        img:'https://cdn.cellphones.com.vn/media/catalog/product/cache/7/small_image/450x/9df78eab33525d08d6e5fb8d27136e95/i/p/iphone_x_64gb_3.jpg'
+      },
+      {
+        id: 4,
+        name: "3",
+        price: 25,
+        desc:'Lorme',
+        status:true,
+        img:'https://cdn.cellphones.com.vn/media/catalog/product/cache/7/small_image/450x/9df78eab33525d08d6e5fb8d27136e95/i/p/iphone_x_64gb_3.jpg'
+      },
+      {
+        id: 5,
+        name: "4",
+        price: 25,
+        desc:'Lorme',
+        status:true,
+        img:'https://cdn.cellphones.com.vn/media/catalog/product/cache/7/small_image/450x/9df78eab33525d08d6e5fb8d27136e95/i/p/iphone_x_64gb_3.jpg'
+      },
+      {
+        id: 6,
+        name: "5",
+        price: 25,
+        desc:'Lorme',
+        status:true,
+        img:'https://cdn.cellphones.com.vn/media/catalog/product/cache/7/small_image/450x/9df78eab33525d08d6e5fb8d27136e95/i/p/iphone_8_plus_256gb_1_1.jpg'
+      },  {
+        id: 7,
+        name: "6",
+        price: 25,
+        desc:'Lorme',
+        status:true,
+        img:'https://techland.com.vn/wp-content/uploads/2019/09/apple-iphone-11-7.jpg'
+      }
 ]
