40b2f9816d6748a9d2e3cf3db32b9fd24f725f53
yenvy0512 <kieuyenvy0512@gmail.com>
2020-03-19 22:16:11 +0700

    5 files modified

--- src/app/MockData.ts
+++ src/app/MockData.ts
@@ -3,49 +3,79 @@ import { Product } from './Product';
 export const data : Product[] = [
     {
         id: 1,
-        name: "1",
+        name: "Iphone 7",
         price: 24,
-        desc:'Lorme',
+        desc:'256G',
         status:false,
         img:'https://techland.com.vn/wp-content/uploads/2019/09/apple-iphone-11-7.jpg'
       },
       {
-        id: 3,
-        name: "2",
+        id: 2,
+        name: "Iphone 11",
         price: 25,
-        desc:'Lorme',
+        desc:'128G',
+        status:true,
+        img:'https://winmobile.vn/images/thumbs/2020/01/iphone-8-plus-64gb-quoc-te-97-290.jpg'
+      },
+      {
+        id: 3,
+        name: "Iphone 8",
+        price: 25,
+        desc:'128G',
         status:true,
         img:'https://cdn.cellphones.com.vn/media/catalog/product/cache/7/small_image/450x/9df78eab33525d08d6e5fb8d27136e95/i/p/iphone_x_64gb_3.jpg'
       },
       {
         id: 4,
-        name: "3",
+        name: "Iphone 11 ProMax",
         price: 25,
-        desc:'Lorme',
+        desc:'256G',
         status:true,
         img:'https://cdn.cellphones.com.vn/media/catalog/product/cache/7/small_image/450x/9df78eab33525d08d6e5fb8d27136e95/i/p/iphone_x_64gb_3.jpg'
       },
       {
         id: 5,
-        name: "4",
-        price: 25,
-        desc:'Lorme',
-        status:true,
-        img:'https://cdn.cellphones.com.vn/media/catalog/product/cache/7/small_image/450x/9df78eab33525d08d6e5fb8d27136e95/i/p/iphone_x_64gb_3.jpg'
-      },
-      {
-        id: 6,
-        name: "5",
+        name: "Iphone 8",
         price: 25,
         desc:'Lorme',
         status:true,
         img:'https://cdn.cellphones.com.vn/media/catalog/product/cache/7/small_image/450x/9df78eab33525d08d6e5fb8d27136e95/i/p/iphone_8_plus_256gb_1_1.jpg'
       },  {
-        id: 7,
-        name: "6",
+        id: 6,
+        name: "Iphone X",
         price: 25,
-        desc:'Lorme',
+        desc:'64GG',
         status:true,
         img:'https://techland.com.vn/wp-content/uploads/2019/09/apple-iphone-11-7.jpg'
+      },
+       {
+        id: 7,
+        name: "Iphone 8",
+        price: 25,
+        desc:'128G',
+        status:true,
+        img:'https://cdn.cellphones.com.vn/media/catalog/product/cache/7/small_image/450x/9df78eab33525d08d6e5fb8d27136e95/i/p/iphone_8_plus_256gb_1_1.jpg'
+      },
+      {
+        id: 10,
+        name: "Iphone 11",
+        price: 25,
+        desc:'128G',
+        status:true,
+        img:'https://winmobile.vn/images/thumbs/2020/01/iphone-8-plus-64gb-quoc-te-97-290.jpg'
+      },  {
+        id: 8,
+        name: "Iphone 8",
+        price: 25,
+        desc:'128G',
+        status:true,
+        img:'https://cdn.cellphones.com.vn/media/catalog/product/cache/7/small_image/450x/9df78eab33525d08d6e5fb8d27136e95/i/p/iphone_x_64gb_3.jpg'
+      },{
+        id: 9,
+        name: "Iphone 8",
+        price: 25,
+        desc:'64G',
+        status:true,
+        img:'https://cdn.cellphones.com.vn/media/catalog/product/cache/7/small_image/450x/9df78eab33525d08d6e5fb8d27136e95/i/p/iphone_8_plus_256gb_1_1.jpg'
       }
 ]
