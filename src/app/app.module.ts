1cecad1d4730f2d023230193716c064b9818af91
yenvy0512 <kieuyenvy0512@gmail.com>
2020-03-29 15:41:24 +0700

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
@@ -18,7 +19,7 @@ import { NewService } from './new.service';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ,AppRoutingModule,HttpClientModule],
+  imports:      [ BrowserModule, FormsModule ,AppRoutingModule,HttpClientModule,NgbModule],
   declarations: [ AppComponent, HelloComponent, ProductComponent, ProductListComponent, ProductAddComponent, CategoryComponent, SliderComponent, HomeComponent, ProductDetailComponent, NewListComponent ],
   bootstrap:    [ AppComponent ],
   providers: [ProductService, NewService]
